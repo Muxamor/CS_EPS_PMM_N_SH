a3d8188c1c07a4adc9ebbd0f8773e981b228452f
Ivan Neskorodev <ivan.neskorodev@gmail.com>
2020-06-18 19:14:24 +0700

    Modif PDM struct add flag save data

--- Core/Inc/PDM/pdm_struct.h
+++ Core/Inc/PDM/pdm_struct.h
@@ -10,10 +10,10 @@ typedef struct{
 	int16_t Current_val;
 	uint16_t Power_val;
 
-	uint8_t State_eF_in  :1; //0-DISABLE, 1-ENABLE. State power channel input Efuse
-	uint8_t State_eF_out :1; //0-DISABLE, 1-ENABLE. State power channel output Efuse
-	uint8_t PG_eF_in  	 :1; //0-OK, 1-ERROR. Power good channel status input eFuse  
-	uint8_t PG_eF_out 	 :1; //0-OK, 1-ERROR. Power good channel status output eFuse
+	uint8_t State_eF_in  		:1; //0-DISABLE, 1-ENABLE. State power channel input Efuse
+	uint8_t State_eF_out 		:1; //0-DISABLE, 1-ENABLE. State power channel output Efuse
+	uint8_t PG_eF_in			:1; //0-OK, 1-ERROR. Power good channel status input eFuse
+	uint8_t PG_eF_out 	 		:1; //0-OK, 1-ERROR. Power good channel status output eFuse
 	uint8_t :4;
 
 	uint8_t Error_State_eF_in  	:1; //0-OK, 1-ERROR.
@@ -25,15 +25,9 @@ typedef struct{
 
 typedef struct{
 
-
 	_PDM_PWR_CH PWR_Channel[PDM_PWR_Ch_quantity];
 
 	int8_t Temp_sensor[4]; //PDM temperature
-	//int8_t Temp_sensor_1; //PDM temperature
-	//int8_t Temp_sensor_2;
-	//int8_t Temp_sensor_3;
-	//int8_t Temp_sensor_4;
-	int8_t Temp_median_value; //PDM median temperature
 
 	uint8_t Error_I2C_GPIO_Ext1 	:1;
 	uint8_t Error_I2C_GPIO_Ext2 	:1;
@@ -44,6 +38,9 @@ typedef struct{
 	uint8_t Error_temp_sensor_4		:1;
 	uint8_t :1;
 	
+	uint8_t PDM_save_conf_flag  	:1;// 0-Note need save, 1-Need save configure PDM to FRAM.
+	uint8_t :7;
+
 	uint8_t PDM_addres_module;
 
 }_PDM;
