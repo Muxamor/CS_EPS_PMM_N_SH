753ffa600ea327695a3602f784ab1014d4f2a50c
Ivan Neskorodev <ivan.neskorodev@gmail.com>
2020-06-18 19:16:28 +0700

    Add calculate CRC16

---
+++ Core/Inc/Fn_CRC16.h
@@ -0,0 +1,9 @@
+
+#ifndef INC_FN_CRC16_H_
+#define INC_FN_CRC16_H_
+
+unsigned short Crc16(unsigned char * pcBlock, unsigned short len);
+uint16_t Crc16_1021(uint8_t * pcBlock, uint16_t len);
+
+
+#endif /* INC_FN_CRC16_H_ */
