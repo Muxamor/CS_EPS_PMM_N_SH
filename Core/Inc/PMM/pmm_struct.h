625b8e125205de8d17adb80f98326905d4f607ac
Ivan Neskorodev <ivan.neskorodev@gmail.com>
2020-06-23 19:33:41 +0700

    change pmm struct

--- Core/Inc/PMM/pmm_struct.h
+++ Core/Inc/PMM/pmm_struct.h
@@ -31,29 +31,39 @@ typedef struct{
 	int16_t  PWR_Ch_Vbat2_eF1_Current_val;
 	int16_t  PWR_Ch_Vbat2_eF2_Current_val;
 	uint16_t PWR_Ch_Vbat2_eF1_Power_val;
-	uint16_t PWR_Ch_Vbat2_eF2_Power_val;
+	uint16_t PWR_Ch_Vbat2_eF2_Power_val; 
 
 	int16_t  PWR_Supply_Main_eF_in_Current_val;
 	int16_t  PWR_Supply_Maim_eF_out_Current_val;
 	int16_t  PWR_Supply_Backup_eF_in_Current_val;
 	int16_t  PWR_Supply_Backup_eF_out_Current_val;
 	
-	uint16_t PWR_Ch_State_CANmain				:1; //0-DISABLE, 1-ENABLE. State power main CAN.
-	uint16_t PWR_Ch_State_CANbackup				:1; //0-DISABLE, 1-ENABLE. State power backup CAN.
-	uint16_t PWR_Ch_State_Vbat1_eF1   			:1; //0-DISABLE, 1-ENABLE. State power battery bus output 1 eFuse 1 
-	uint16_t PWR_Ch_State_Vbat1_eF2				:1; //0-DISABLE, 1-ENABLE. State power battery bus output 1 eFuse 2 
-	uint16_t PWR_Ch_State_Vbat2_eF1     		:1; //0-DISABLE, 1-ENABLE. State power battery bus output 2 eFuse 1 
-	uint16_t PWR_Ch_State_Vbat2_eF2				:1; //0-DISABLE, 1-ENABLE. State power battery bus output 2 eFuse 2
-	uint16_t PWR_Ch_State_PBMs_Logic			:1; //0-DISABLE, 1-ENABLE. State power PBM -modules logic power 
-	uint16_t PWR_Ch_State_Deploy_Logic 			:1; //0-DISABLE, 1-ENABLE. State power Deploy logic 
-	uint16_t PWR_Ch_State_Deploy_Power 			:1; //0-DISABLE, 1-ENABLE. State power Deploy power 
-	uint16_t PWR_Ch_State_5V_Bus 				:1; //0-DISABLE, 1-ENABLE. State power 5V bus 
-	uint16_t PWR_Ch_State_3_3V_Bus 				:1; //0-DISABLE, 1-ENABLE. State power 3.3V bus 
-	uint16_t PWR_Ch_PG_Vbat1_eF1 				:1; //0-OK, 1-ERROR. Power good VBAT1 eF1 channel status.
-	uint16_t PWR_Ch_PG_Vbat1_eF2 				:1; //0-OK, 1-ERROR. Power good VBAT1 eF2 channel status.
-	uint16_t PWR_Ch_PG_Vbat2_eF1 				:1; //0-OK, 1-ERROR. Power good VBAT2 eF1 channel status.
-	uint16_t PWR_Ch_PG_Vbat2_eF2 				:1; //0-OK, 1-ERROR. Power good VBAT2 eF2 channel status.
-	uint16_t :1;
+	uint32_t PWR_Ch_State_CANmain				:1; //0-DISABLE, 1-ENABLE. State power main CAN.
+	uint32_t PWR_Ch_State_CANbackup				:1; //0-DISABLE, 1-ENABLE. State power backup CAN.
+	uint32_t PWR_Ch_State_Vbat1_eF1   			:1; //0-DISABLE, 1-ENABLE. State power battery bus output 1 eFuse 1 
+	uint32_t PWR_Ch_State_Vbat1_eF2				:1; //0-DISABLE, 1-ENABLE. State power battery bus output 1 eFuse 2 
+	uint32_t PWR_Ch_State_Vbat2_eF1     		:1; //0-DISABLE, 1-ENABLE. State power battery bus output 2 eFuse 1 
+	uint32_t PWR_Ch_State_Vbat2_eF2				:1; //0-DISABLE, 1-ENABLE. State power battery bus output 2 eFuse 2
+	uint32_t PWR_Ch_State_PBMs_Logic			:1; //0-DISABLE, 1-ENABLE. State power PBM -modules logic power 
+	uint32_t PWR_Ch_State_Deploy_Logic 			:1; //0-DISABLE, 1-ENABLE. State power Deploy logic 
+	uint32_t PWR_Ch_State_Deploy_Power 			:1; //0-DISABLE, 1-ENABLE. State power Deploy power 
+	uint32_t PWR_Ch_State_5V_Bus 				:1; //0-DISABLE, 1-ENABLE. State power 5V bus (Norbi not use)
+	uint32_t PWR_Ch_State_3_3V_Bus 				:1; //0-DISABLE, 1-ENABLE. State power 3.3V bus (Norbi not use)
+	uint32_t PWR_Ch_State_I2C_Bus 				:1; //0-DISABLE, 1-ENABLE. State power I2C bus.(Norbi not use)
+	uint32_t PWR_Supply_Main_PG 				:1; //0-OK, 1-ERROR. Power good Main power supply 3.3V
+	uint32_t PWR_Supply_Backup_PG 				:1; //0-OK, 1-ERROR. Power good Backup power supply 3.3V
+	uint32_t PWR_Ch_PG_CANmain					:1; //0-OK, 1-ERROR. Power good main CAN power ch.
+	uint32_t PWR_Ch_PG_CANbackup				:1; //0-OK, 1-ERROR. Power good backup CAN power ch.
+	uint32_t PWR_Ch_PG_Vbat1_eF1 				:1; //0-OK, 1-ERROR. Power good VBAT1 eF1 channel status.
+	uint32_t PWR_Ch_PG_Vbat1_eF2 				:1; //0-OK, 1-ERROR. Power good VBAT1 eF2 channel status.
+	uint32_t PWR_Ch_PG_Vbat2_eF1 				:1; //0-OK, 1-ERROR. Power good VBAT2 eF1 channel status.
+	uint32_t PWR_Ch_PG_Vbat2_eF2 				:1; //0-OK, 1-ERROR. Power good VBAT2 eF2 channel status.
+	uint32_t PWR_Ch_PG_PBMs_Logic 				:1; //0-OK, 1-ERROR. Power good PBM logic power.
+	uint32_t PWR_Ch_PG_PBMs_Deploy_LP			:1; //0-OK, 1-ERROR. Power good Deploy power. (Norbi burning eFuse)
+	uint32_t PWR_Ch_PG_5V_Bus 					:1; //0-OK, 1-ERROR. Power good 5V power. (Norbi not use)
+	uint32_t PWR_Ch_PG_3_3V_Bus 				:1; //0-OK, 1-ERROR. Power good 3.3V power. (Norbi not use)
+	uint32_t PWR_Ch_PG_I2C_Bus 					:1; //0-OK, 1-ERROR. Power good I2C power. (Norbi not use)
+	uint32_t :7;
 
 
 	uint32_t Error_TMP1075_sensor				:1;	//=0 -OK, = 1-error temp. sensor TMP1075
