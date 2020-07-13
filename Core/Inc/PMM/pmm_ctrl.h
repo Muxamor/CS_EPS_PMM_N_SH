7c7cc69f449722a30b037e63ed27440653d3129a
Ivan Neskorodev <ivan.neskorodev@gmail.com>
2020-07-13 20:35:43 +0700

    Update pmm_ctrl.h

--- Core/Inc/PMM/pmm_ctrl.h
+++ Core/Inc/PMM/pmm_ctrl.h
@@ -11,7 +11,7 @@ ErrorStatus PMM_Get_PWR_CH_VBAT_I_V_P( _PMM *pmm_ptr, uint8_t num_pwr_ch);
 ErrorStatus PMM_ADS1015_average_meas(I2C_TypeDef *I2Cx, uint8_t I2C_ADS1015_addr, uint8_t num_ch_mux, uint8_t average_num, float *average_data);
 ErrorStatus PMM_Get_PWR_Supply_m_b_I( _PMM *pmm_ptr, I2C_TypeDef *I2Cx, uint8_t I2C_ADS1015_addr);
 
-ErrorStatus PMM_Set_MUX_CAN_CPUm_CPUb( uint8_t active_CPU );
+ErrorStatus PMM_Set_MUX_CAN_CPUm_CPUb( _PMM *pmm_ptr );
 
 
 #endif /* INC_PMM_PMM_CTRL_H_ */
