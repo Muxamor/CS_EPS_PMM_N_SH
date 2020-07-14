7e3196b9c61739114c816191055b09b649b115e3
Ivan Neskorodev <ivan.neskorodev@gmail.com>
2020-07-14 21:48:41 +0700

    Fix  to build

--- Core/Inc/PMM/pmm_ctrl.h
+++ Core/Inc/PMM/pmm_ctrl.h
@@ -12,7 +12,7 @@ ErrorStatus PMM_ADS1015_average_meas(I2C_TypeDef *I2Cx, uint8_t I2C_ADS1015_addr
 ErrorStatus PMM_Get_PWR_Supply_m_b_I( _PMM *pmm_ptr, I2C_TypeDef *I2Cx, uint8_t I2C_ADS1015_addr);
 
 ErrorStatus PMM_Set_MUX_CAN_CPUm_CPUb( _PMM *pmm_ptr );
-ErrorStatus PMM_Switch_Active_CPU(uint8_t set_active_CPU,  _UART_EPS_COMM *UART_Main_eps_comm, _UART_EPS_COMM *UART_Backup_eps_comm, _PMM *pmm_ptr,_PDM *pdm_ptr, _PBM pbm_mas[])
+//ErrorStatus PMM_Switch_Active_CPU(uint8_t set_active_CPU,  _UART_EPS_COMM *UART_Main_eps_comm, _UART_EPS_COMM *UART_Backup_eps_comm, _PMM *pmm_ptr,_PDM *pdm_ptr, _PBM pbm_mas[]);
 
 
 #endif /* INC_PMM_PMM_CTRL_H_ */
