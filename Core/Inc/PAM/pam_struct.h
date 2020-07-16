916b67d0334626461bbff1a6163f86f657b12cc0
Ivan Neskorodev <ivan.neskorodev@gmail.com>
2020-07-16 17:18:54 +0700

    add draft PAM struct

---
+++ Core/Inc/PAM/pam_struct.h
@@ -0,0 +1,38 @@
+
+#ifndef INC_PAM_PAM_STRUCT_H_
+#define INC_PAM_PAM_STRUCT_H_
+
+
+
+//#pragma pack(push, 1)
+
+typedef struct{
+	
+	uint8_t Some_byte1;
+	uint8_t Some_byte2;
+
+}_PAM_SP;
+
+
+typedef struct{
+	
+	uint8_t Some_byte1;
+	uint8_t Some_byte2;
+
+}_PAM_PWR_CH;
+
+typedef struct{
+
+	uint8_t Some_byte1;
+	
+	uint8_t :7;
+	uint8_t PAM_save_conf_flag :1;
+
+	uint16_t reserved1;
+	uint16_t reserved2;
+
+}_PAM;
+
+//#pragma pack(pop)
+
+#endif /* INC_PDM_PDM_STRUCT_H_ */
