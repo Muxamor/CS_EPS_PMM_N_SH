49f2ab3c9254dcbefecb229f4730202cab9426d9
Ivan Neskorodev <ivan.neskorodev@gmail.com>
2020-07-20 19:28:40 +0700

    PDM editing grammar

--- Core/Src/PDM/pdm.c
+++ Core/Src/PDM/pdm.c
@@ -12,35 +12,35 @@
 */
 ErrorStatus PDM_Get_Telemetry( _PDM *pdm_ptr ){
 
-	int8_t error_stutus = SUCCESS;
+	int8_t error_status = SUCCESS;
 	uint8_t num_pwr_ch = 0;
 
 	//Check GPIO eFuse state.
 	for(num_pwr_ch = 0; num_pwr_ch < PDM_PWR_Ch_quantity; num_pwr_ch++){
 
-		error_stutus += PDM_Check_state_PWR_CH( pdm_ptr, num_pwr_ch);
+		error_status += PDM_Check_state_PWR_CH( pdm_ptr, num_pwr_ch);
 	}
 
 
 	//Check GPIO eFuse state.
 	for(num_pwr_ch = 0; num_pwr_ch < PDM_PWR_Ch_quantity; num_pwr_ch++){
 
-		error_stutus += PDM_Get_PG_PWR_CH( pdm_ptr, num_pwr_ch);
+		error_status += PDM_Get_PG_PWR_CH( pdm_ptr, num_pwr_ch);
 	}
 
 	//Get value Current, Voltage, Power
 	for(num_pwr_ch = 0; num_pwr_ch < PDM_PWR_Ch_quantity; num_pwr_ch++){
 
-		error_stutus += PDM_Get_PWR_CH_I_V_P( pdm_ptr, num_pwr_ch);
+		error_status += PDM_Get_PWR_CH_I_V_P( pdm_ptr, num_pwr_ch);
 	}
 
 	//Get temperature value.
-	error_stutus += PDM_Get_Temperature( pdm_ptr, PDM_I2Cx_TMP1075_1, PDM_I2CADDR_TMP1075_1, PDM_I2CADDR_I2C_MUX, TCA9548_CH6 );
-	error_stutus += PDM_Get_Temperature( pdm_ptr, PDM_I2Cx_TMP1075_2, PDM_I2CADDR_TMP1075_2, PDM_I2CADDR_I2C_MUX, TCA9548_CH6 );
-	error_stutus += PDM_Get_Temperature( pdm_ptr, PDM_I2Cx_TMP1075_3, PDM_I2CADDR_TMP1075_3, PDM_I2CADDR_I2C_MUX, TCA9548_CH6 );
-	error_stutus += PDM_Get_Temperature( pdm_ptr, PDM_I2Cx_TMP1075_4, PDM_I2CADDR_TMP1075_4, PDM_I2CADDR_I2C_MUX, TCA9548_CH6 );
+	error_status += PDM_Get_Temperature( pdm_ptr, PDM_I2Cx_TMP1075_1, PDM_I2CADDR_TMP1075_1, PDM_I2CADDR_I2C_MUX, TCA9548_CH6 );
+	error_status += PDM_Get_Temperature( pdm_ptr, PDM_I2Cx_TMP1075_2, PDM_I2CADDR_TMP1075_2, PDM_I2CADDR_I2C_MUX, TCA9548_CH6 );
+	error_status += PDM_Get_Temperature( pdm_ptr, PDM_I2Cx_TMP1075_3, PDM_I2CADDR_TMP1075_3, PDM_I2CADDR_I2C_MUX, TCA9548_CH6 );
+	error_status += PDM_Get_Temperature( pdm_ptr, PDM_I2Cx_TMP1075_4, PDM_I2CADDR_TMP1075_4, PDM_I2CADDR_I2C_MUX, TCA9548_CH6 );
 
-	if(error_stutus != SUCCESS){
+	if(error_status != SUCCESS){
 		return ERROR_N;
 	}
 
--- Core/Src/PDM/pdm_init.c
+++ Core/Src/PDM/pdm_init.c
@@ -9,23 +9,23 @@
 
 
 
-/** @brief  Inicialisation PDM board
+/** @brief  Initialisation PDM board
 	@param  *pdm_ptr - pointer to struct which contain all information about PDM.
 	@retval 0 - SUCCESS, -1 - ERROR_N
 */
 ErrorStatus PDM_init(_PDM *pdm_ptr){
 
-	int8_t error_stutus = SUCCESS; 
-	uint8_t num_pwr_ch = 0;
+	int8_t error_status = SUCCESS;
+	uint8_t num_pwr_ch;
 	
 
 	//Restoring power channels.
 	for(num_pwr_ch = 0; num_pwr_ch < PDM_PWR_Ch_quantity; num_pwr_ch++){
 
 		if( (pdm_ptr->PWR_Channel[num_pwr_ch].State_eF_in == ENABLE) && (pdm_ptr->PWR_Channel[num_pwr_ch].State_eF_out == ENABLE) ){
-			error_stutus += PDM_Set_state_PWR_CH( pdm_ptr, num_pwr_ch, ENABLE );
+			error_status += PDM_Set_state_PWR_CH( pdm_ptr, num_pwr_ch, ENABLE );
 		}else{
-			error_stutus += PDM_Set_state_PWR_CH( pdm_ptr, num_pwr_ch, DISABLE );
+			error_status += PDM_Set_state_PWR_CH( pdm_ptr, num_pwr_ch, DISABLE );
 		}
 	}
 
@@ -36,19 +36,19 @@ ErrorStatus PDM_init(_PDM *pdm_ptr){
 
 
 	//Initialization temperature sensors.
-	error_stutus += PDM_init_TMP1075( pdm_ptr, PDM_I2Cx_TMP1075_1, PDM_I2CADDR_TMP1075_1, PDM_I2CADDR_I2C_MUX, TCA9548_CH6 );
-	error_stutus += PDM_init_TMP1075( pdm_ptr, PDM_I2Cx_TMP1075_2, PDM_I2CADDR_TMP1075_2, PDM_I2CADDR_I2C_MUX, TCA9548_CH6 );
-	error_stutus += PDM_init_TMP1075( pdm_ptr, PDM_I2Cx_TMP1075_3, PDM_I2CADDR_TMP1075_3, PDM_I2CADDR_I2C_MUX, TCA9548_CH6 );
-	error_stutus += PDM_init_TMP1075( pdm_ptr, PDM_I2Cx_TMP1075_4, PDM_I2CADDR_TMP1075_4, PDM_I2CADDR_I2C_MUX, TCA9548_CH6 );
+	error_status += PDM_init_TMP1075( pdm_ptr, PDM_I2Cx_TMP1075_1, PDM_I2CADDR_TMP1075_1, PDM_I2CADDR_I2C_MUX, TCA9548_CH6 );
+	error_status += PDM_init_TMP1075( pdm_ptr, PDM_I2Cx_TMP1075_2, PDM_I2CADDR_TMP1075_2, PDM_I2CADDR_I2C_MUX, TCA9548_CH6 );
+	error_status += PDM_init_TMP1075( pdm_ptr, PDM_I2Cx_TMP1075_3, PDM_I2CADDR_TMP1075_3, PDM_I2CADDR_I2C_MUX, TCA9548_CH6 );
+	error_status += PDM_init_TMP1075( pdm_ptr, PDM_I2Cx_TMP1075_4, PDM_I2CADDR_TMP1075_4, PDM_I2CADDR_I2C_MUX, TCA9548_CH6 );
 
 
 	//Initialization Power monitor INA231 power channel
 	for(num_pwr_ch = 0; num_pwr_ch < PDM_PWR_Ch_quantity; num_pwr_ch++){
-		error_stutus += PDM_init_PWR_Mon_INA231( pdm_ptr, num_pwr_ch);
+		error_status += PDM_init_PWR_Mon_INA231( pdm_ptr, num_pwr_ch);
 	}
 
 
-	if(error_stutus != SUCCESS){
+	if(error_status != SUCCESS){
 		#ifdef DEBUGprintf
 			Error_Handler();
 		#endif
