06dba893e4e2b7a282a64dee9e6f06851eeda29a
Ivan Neskorodev <ivan.neskorodev@gmail.com>
2020-07-23 16:41:54 +0700

    Add littel PBM

---
+++ Core/Inc/PBM/pbm_control.h
@@ -0,0 +1,37 @@
+
+#ifndef INC_PBM_CONTROL_H_
+#define INC_PBM_CONTROL_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+ErrorStatus PBM_ReadGPIO(I2C_TypeDef *I2Cx, _PBM pbm[], uint8_t PBM_number);
+
+ErrorStatus PBM_ReadTempSensors(I2C_TypeDef *I2Cx, _PBM pbm[], uint8_t PBM_number);
+
+ErrorStatus PBM_ReadBatteryData(I2C_TypeDef *I2Cx, _PBM pbm[], uint8_t PBM_number);
+
+ErrorStatus PBM_SetStateHeatBranch(I2C_TypeDef *I2Cx, _PBM pbm[], uint8_t PBM_number, uint8_t Branch, uint8_t State);
+
+ErrorStatus PBM_SetStateChargeBranch(I2C_TypeDef *I2Cx, _PBM pbm[], uint8_t PBM_number, uint8_t Branch, uint8_t State);
+
+ErrorStatus PBM_SetStateDischargeBranch(I2C_TypeDef *I2Cx, _PBM pbm[], uint8_t PBM_number, uint8_t Branch, uint8_t State);
+
+ErrorStatus PBM_CheckHeatOFF(_PBM pbm[], uint8_t PBM_number);
+
+ErrorStatus PBM_CheckChargeDischargeState(_PBM pbm[], uint8_t PBM_number);
+
+ErrorStatus PBM_CheckCapacity(I2C_TypeDef *I2Cx, _PBM pbm[], uint8_t PBM_number);
+
+void  PBM_CalcTotalCapacity(_PBM pbm[], uint8_t PBM_number);
+
+void PBM_CheckLevelEnergy(_PBM pbm[], uint8_t PBM_number);
+
+uint8_t PBM_CheckSaveSetupFlag(_PBM pbm[]);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* INC_PBM_control_H_ */
---
+++ Core/Inc/PBM/pbm_init.h
@@ -0,0 +1,21 @@
+
+#ifndef INC_PBM_INIT_H_
+#define INC_PBM_INIT_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "pbm_struct.h"
+
+ErrorStatus PBM_Init(_PBM pbm[]);
+
+ErrorStatus PBM_Re_Init(_PBM pbm[]);
+
+ErrorStatus PBM_TempSensorInit(_PBM pbm[], uint8_t PBM_number);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* INC_PBM_INIT_H_ */
