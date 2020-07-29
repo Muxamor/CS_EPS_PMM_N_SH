6e53dd24807a1bd706858da85fe73089806d9b8d
Ivan Neskorodev <ivan.neskorodev@gmail.com>
2020-07-29 19:15:18 +0700

    fix and clear

--- Core/Inc/PMM/pmm_ctrl.h
+++ Core/Inc/PMM/pmm_ctrl.h
@@ -9,7 +9,7 @@ extern "C" {
 
 ErrorStatus PMM_Set_state_PWR_CH( _PMM *pmm_ptr, uint8_t num_pwr_channel, uint8_t state_channel );
 ErrorStatus PMM_Check_state_PWR_CH( _PMM *pmm_ptr, uint8_t num_pwr_channel );
-ErrorStatus PDM_Get_PG_all_PWR_CH( _PMM *pmm_ptr );
+ErrorStatus PMM_Get_PG_all_PWR_CH( _PMM *pmm_ptr );
 ErrorStatus PMM_Get_Temperature( _PMM *pmm_ptr, I2C_TypeDef *I2Cx, uint8_t tmp1075_addr );
 ErrorStatus PMM_Get_PWR_CH_VBAT_I_V_P( _PMM *pmm_ptr, uint8_t num_pwr_ch);
 
--- Core/Inc/PMM/pmm_init_IC.h
+++ Core/Inc/PMM/pmm_init_IC.h
@@ -7,6 +7,8 @@
 extern "C" {
 #endif
 
+#include "PMM/pmm_struct.h"
+
 ErrorStatus PMM_init_TMP1075(_PMM *pmm_ptr, I2C_TypeDef *I2Cx, uint8_t tmp1075_addr);
 ErrorStatus PMM_Power_Down_TMP1075(_PMM *pmm_ptr, I2C_TypeDef *I2Cx, uint8_t tmp1075_addr);
 ErrorStatus PMM_init_PWR_Mon_INA231( _PMM *pmm_ptr, uint8_t num_pwr_ch);
