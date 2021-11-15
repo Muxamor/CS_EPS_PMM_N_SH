1fc5dae93c190bd5d80c768233fadb7987e90870
Alexander Morsin <a.morsin@5okb.ru>
2021-11-15 17:22:36 +0700

    Change battery struct

--- Core/Src/PBM_T1/pbm_T1.c
+++ Core/Src/PBM_T1/pbm_T1.c
@@ -1,5 +1,6 @@
 #include "stm32l4xx_ll_gpio.h"
 #include "SetupPeriph.h"
+#include "TCA9548.h"
 #include "PBM_T1/pbm_T1_config.h"
 #include "PBM_T1/pbm_T1_struct.h"
 #include "PBM_T1/pbm_T1_control.h"
@@ -13,24 +14,32 @@
  */
 ErrorStatus PBM_T1_Get_Telemetry(_PBM_T1 pbm[]) {
 
-	uint8_t i = 0;
+	uint8_t PBM_Number = 0;
 	uint8_t Low_Energy_Flag_counter = 0;
 	int8_t Error = SUCCESS;
 
-	for (i = 0; i < PBM_T1_QUANTITY; i++) {
-		Error = PBM_T1_ReadBatteryData(PBM_T1_I2C_PORT, pbm, i);
-		Error = Error + PBM_T1_ReadTempSensors(PBM_T1_I2C_PORT, pbm, i);
-		Error = Error + PBM_T1_ReadGPIO(PBM_T1_I2C_PORT, pbm, i);
-		//Error = Error + PBM_CheckCapacity(PBM_T1_I2C_PORT, pbm, i);
-		Error = Error + PBM_T1_CheckHeatOFF(pbm, i);
-		Error = Error + PBM_T1_CheckChargeDischargeState(pbm, i);
+	for (PBM_Number = 0; PBM_Number < PBM_T1_QUANTITY; PBM_Number++) {
 
-        PBM_T1_CalcTotalCapacity(pbm, i);
-        PBM_T1_CheckLevelEnergy(pbm, i);
+		Error = PBM_T1_ReadBatteryTelemetry(PBM_T1_I2C_PORT, pbm, PBM_Number, PBM_T1_BRANCH_1, PBM_T1_I2C_MUX_CH_Br1);
+		Error = PBM_T1_ReadBatteryTelemetry(PBM_T1_I2C_PORT, pbm, PBM_Number, PBM_T1_BRANCH_2, PBM_T1_I2C_MUX_CH_Br2);
+		Error = Error + PBM_T1_ReadTempSensors(PBM_T1_I2C_PORT, pbm, PBM_Number, PBM_T1_TMP1075_ADDR_1, PBM_T1_I2C_MUX_CH_Temp1);
+		Error = Error + PBM_T1_ReadTempSensors(PBM_T1_I2C_PORT, pbm, PBM_Number, PBM_T1_TMP1075_ADDR_2, PBM_T1_I2C_MUX_CH_Temp1);
+		Error = Error + PBM_T1_ReadTempSensors(PBM_T1_I2C_PORT, pbm, PBM_Number, PBM_T1_TMP1075_ADDR_3, PBM_T1_I2C_MUX_CH_Temp2);
+		Error = Error + PBM_T1_ReadTempSensors(PBM_T1_I2C_PORT, pbm, PBM_Number, PBM_T1_TMP1075_ADDR_4, PBM_T1_I2C_MUX_CH_Temp2);
+		Error = Error + PBM_T1_ReadStateTempSense(PBM_T1_I2C_PORT, pbm, PBM_Number, PBM_T1_BRANCH_ALL, PBM_T1_I2C_MUX_CH_GPIO);
+		Error = Error + PBM_T1_ReadStateHeat(PBM_T1_I2C_PORT, pbm, PBM_Number, PBM_T1_BRANCH_ALL, PBM_T1_I2C_MUX_CH_GPIO);
+		Error = Error + PBM_T1_ReadStateEmergChrg(PBM_T1_I2C_PORT, pbm, PBM_Number, PBM_T1_BRANCH_ALL, PBM_T1_I2C_MUX_CH_GPIO);
+		//Error = Error + PBM_CheckCapacity(PBM_T1_I2C_PORT, pbm, PBM_Number);
+		Error = Error + PBM_T1_CheckOverHeat(pbm, PBM_Number, PBM_T1_BRANCH_1, PBM_T1_I2C_MUX_CH_Temp1);
+		Error = Error + PBM_T1_CheckOverHeat(pbm, PBM_Number, PBM_T1_BRANCH_2, PBM_T1_I2C_MUX_CH_Temp2);
+		Error = Error + PBM_T1_CheckChargeDischargeState(pbm, PBM_Number);
+
+        PBM_T1_CalcTotalCapacity(pbm, PBM_Number);
+        PBM_T1_CheckLowLevelEnergy(pbm, PBM_Number);
 	}
 
-	for ( Low_Energy_Flag_counter = 0, i = 0; i < PBM_T1_QUANTITY; i++) {
-		Low_Energy_Flag_counter = Low_Energy_Flag_counter + pbm[i].Low_Energy_Flag;
+	for ( Low_Energy_Flag_counter = 0, PBM_Number = 0; PBM_Number < PBM_T1_QUANTITY; PBM_Number++) {
+		Low_Energy_Flag_counter = Low_Energy_Flag_counter + pbm[PBM_Number].Low_Energy_Flag;
 	}
 
 	if(Low_Energy_Flag_counter >= 1){
