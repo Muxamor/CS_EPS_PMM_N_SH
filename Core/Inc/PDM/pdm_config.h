45fcb32f795841fec647dd1a2344c9f9b1dc8255
Ivan <ivan.neskorodev@gmail.com>
2024-06-14 17:32:53 +0700

    Update PDM I2C port

--- Core/Inc/PDM/pdm_config.h
+++ Core/Inc/PDM/pdm_config.h
@@ -7,73 +7,34 @@ extern "C" {
 
 /*********************** Configuration for PDM  ***********************/
 
-#define PDM_I2Cx_GPIOExt1 			I2C4 //I2C GPIO extender TCA9539 in PDM
-#define PDM_I2CADDR_GPIOExt1 		0x74 //I2C GPIO extender TCA9539 in PDM
+#define PDM_I2Cx_PORT 				I2C4 //I2C GPIO extender TCA9539 in PDM
 
-#define PDM_I2Cx_GPIOExt2 			I2C4 //I2C GPIO extender TCA9539 in PDM
+#define PDM_I2CADDR_GPIOExt1 		0x74 //I2C GPIO extender TCA9539 in PDM
 #define PDM_I2CADDR_GPIOExt2 		0x75//I2C GPIO extender TCA9539 in PDM
 
-
-#define PDM_I2Cx_I2C_MUX1			I2C4 //I2C bus multiplexor TCA9548 in PDM
 #define PDM_I2CADDR_I2C_MUX1		0x70//I2C bus multiplexor TCA9548 in PDM
-
-#define PDM_I2Cx_I2C_MUX2			I2C4 //I2C bus multiplexor TCA9548 in PDM
 #define PDM_I2CADDR_I2C_MUX2		0x71//I2C bus multiplexor TCA9548 in PDM
 
-
-#define PDM_I2Cx_TMP1075_1 			I2C4 //Temp. sensor in PDM 
 #define PDM_I2CADDR_TMP1075_1 		0x48 //Temp. sensor in PDM
-
-#define PDM_I2Cx_TMP1075_2 			I2C4 //Temp. sensor in PDM
 #define PDM_I2CADDR_TMP1075_2 		0x49 //Temp. sensor in PDM
-
-#define PDM_I2Cx_TMP1075_3 			I2C4 //Temp. sensor in PDM
 #define PDM_I2CADDR_TMP1075_3 		0x4A //Temp. sensor in PDM
-
-#define PDM_I2Cx_TMP1075_4 			I2C4 //Temp. sensor in PDM
 #define PDM_I2CADDR_TMP1075_4 		0x4B //Temp. sensor in PDM
 
-
-#define PDM_I2Cx_PWR_Mon_Ch1 		I2C4 //Power Monitor INA231 power channel 1 in PDM H1H2
 #define PDM_I2CADDR_PWR_Mon_Ch1 	0x40 //Power Monitor INA231 power channel 1 in PDM H1H2
-
-#define PDM_I2Cx_PWR_Mon_Ch2  		I2C4 //Power Monitor INA231 power channel 2 in PDM H1H2
 #define PDM_I2CADDR_PWR_Mon_Ch2 	0x41 //Power Monitor INA231 power channel 2 in PDM H1H2
-
-#define PDM_I2Cx_PWR_Mon_Ch3  		I2C4 //Power Monitor INA231 power channel 3 in PDM H1H2
 #define PDM_I2CADDR_PWR_Mon_Ch3  	0x42 //Power Monitor INA231 power channel 3 in PDM H1H2
-
-#define PDM_I2Cx_PWR_Mon_Ch4  		I2C4 //Power Monitor INA231 power channel 4 in PDM H1H2
 #define PDM_I2CADDR_PWR_Mon_Ch4 	0x43 //Power Monitor INA231 power channel 4 in PDM H1H2
-
-#define PDM_I2Cx_PWR_Mon_Ch5  		I2C4 //Power Monitor INA231 power channel 5 in PDM H1H2
 #define PDM_I2CADDR_PWR_Mon_Ch5 	0x44 //Power Monitor INA231 power channel 5 in PDM H1H2
-
-#define PDM_I2Cx_PWR_Mon_Ch6  		I2C4 //Power Monitor INA231 power channel 6 in PDM H1H2
 #define PDM_I2CADDR_PWR_Mon_Ch6 	0x45 //Power Monitor INA231 power channel 6 in PDM H1H2
-
-#define PDM_I2Cx_PWR_Mon_Ch7  		I2C4 //Power Monitor INA231 power channel 7 in PDM H1H2
 #define PDM_I2CADDR_PWR_Mon_Ch7 	0x46 //Power Monitor INA231 power channel 7 in PDM H1H2
-
-#define PDM_I2Cx_PWR_Mon_Ch8  		I2C4 //Power Monitor INA231 power channel 8 in PDM Wired channel №1
 #define PDM_I2CADDR_PWR_Mon_Ch8 	0x47 //Power Monitor INA231 power channel 8 in PDM Wired channel №1
-
-#define PDM_I2Cx_PWR_Mon_Ch9  		I2C4 //Power Monitor INA231 power channel 8 in PDM Wired channel №2
 #define PDM_I2CADDR_PWR_Mon_Ch9 	0x48 //Power Monitor INA231 power channel 8 in PDM Wired channel №2
-
-#define PDM_I2Cx_PWR_Mon_Ch10  		I2C4 //Power Monitor INA231 power channel 8 in PDM Wired channel №3
 #define PDM_I2CADDR_PWR_Mon_Ch10 	0x49 //Power Monitor INA231 power channel 8 in PDM Wired channel №3
-
-#define PDM_I2Cx_PWR_Mon_Ch11  		I2C4 //Power Monitor INA231 power channel 8 in PDM Wired channel №4
 #define PDM_I2CADDR_PWR_Mon_Ch11 	0x4A //Power Monitor INA231 power channel 8 in PDM Wired channel №4
-
-#define PDM_I2Cx_PWR_Mon_Ch12  		I2C4 //Power Monitor INA231 power channel 8 in PDM High Current Channel channel №1
 #define PDM_I2CADDR_PWR_Mon_Ch12 	0x4B //Power Monitor INA231 power channel 8 in PDM High Current Channel channel №1
-
 
 #define pdm_i2c_attempt_conn		3 	 // Quantity attempt to connection through I2C
 #define pdm_i2c_delay_att_conn		3 	 //ms Delay between attempts connection through I2C in millisecond
-
 
 //Later transfer to structure PDM
 #define PDM_PCB_Assembled_PWR_CH1 		0x01 // 1 - PWR channel is assembled on the PCB, 0 - no assembled
--- Core/Src/PDM/pdm_config.c
+++ Core/Src/PDM/pdm_config.c
@@ -8,25 +8,25 @@ _PDM_table PDM__Table(uint8_t number_pwr_channel){
 
 	_PDM_table pdm_table = {0};
 
+	pdm_table.I2Cx_GPIO_Ext = PDM_I2Cx_PORT;
+	pdm_table.I2Cx_I2C_MUX = PDM_I2Cx_PORT;
+	pdm_table.I2Cx_PWR_Mon = PDM_I2Cx_PORT;
 
 	switch(number_pwr_channel){
 
 		case PDM_PWR_Channel_1:
 
 			//Config I2C GPIO Expander TCA9539 for power channel 1
-			pdm_table.I2Cx_I2C_MUX = PDM_I2Cx_I2C_MUX1;
 			pdm_table.I2C_addr_I2C_MUX = PDM_I2CADDR_I2C_MUX1;
 			//Config I2C MUX for power channel 1
 			pdm_table.I2C_MUX_Ch = TCA9548_CH0;
 
-			//Config I2C GPIO Expander TCA9539 for power channel 1
-			pdm_table.I2Cx_GPIO_Ext = PDM_I2Cx_GPIOExt1;
+			//Config I2C GPIO Expander TCA9539 for power channel
 			pdm_table.I2C_addr_GPIO_Ext = PDM_I2CADDR_GPIOExt1;
 			pdm_table.pin_EN_eFuse = TCA9539_IO_P00;			//EN_eFusee_in_ch1 (U25)
 			pdm_table.pin_PG_eFuse = TCA9539_IO_P01;			//PG_eFusee_in_ch1 (U25)
 
 			//Config Power monitor INA238 for power channel 1
-			pdm_table.I2Cx_PWR_Mon = PDM_I2Cx_PWR_Mon_Ch1;
 			pdm_table.I2C_addr_PWR_Mon = PDM_I2CADDR_PWR_Mon_Ch1;
 
 			//Config Power monitor INA238, same for all power channels
@@ -43,20 +43,17 @@ _PDM_table PDM__Table(uint8_t number_pwr_channel){
 
 		case PDM_PWR_Channel_2:
 			//Config I2C GPIO Expander TCA9539 for power channel 2
-			pdm_table.I2Cx_I2C_MUX = PDM_I2Cx_I2C_MUX2;
 			pdm_table.I2C_addr_I2C_MUX = PDM_I2CADDR_I2C_MUX2;
 			//Config I2C MUX for power channel 1
 			pdm_table.I2C_MUX_Ch = TCA9548_CH0;
 
 
 			//Config I2C GPIO Expander TCA9539 for power channel 2
-			pdm_table.I2Cx_GPIO_Ext = PDM_I2Cx_GPIOExt2;
 			pdm_table.I2C_addr_GPIO_Ext = PDM_I2CADDR_GPIOExt2;
 			pdm_table.pin_EN_eFuse = TCA9539_IO_P00;			//EN_eFusee_in_ch2 (U20)
 			pdm_table.pin_PG_eFuse = TCA9539_IO_P01;			//PG_eFusee_in_ch2 (U20)
 
 			//Config Power monitor INA238 for power channel 2
-			pdm_table.I2Cx_PWR_Mon = PDM_I2Cx_PWR_Mon_Ch2;
 			pdm_table.I2C_addr_PWR_Mon = PDM_I2CADDR_PWR_Mon_Ch2;
 
 			//Config Power monitor INA238, same for all power channels
@@ -73,19 +70,16 @@ _PDM_table PDM__Table(uint8_t number_pwr_channel){
 
 		case PDM_PWR_Channel_3:
 			//Config I2C GPIO Expander TCA9539 for power channel 3
-			pdm_table.I2Cx_I2C_MUX = PDM_I2Cx_I2C_MUX1;
 			pdm_table.I2C_addr_I2C_MUX = PDM_I2CADDR_I2C_MUX1;
 			//Config I2C MUX for power channel 1
 			pdm_table.I2C_MUX_Ch = TCA9548_CH1;
 
 			//Config I2C GPIO Expander TCA9539 for power channel 3
-			pdm_table.I2Cx_GPIO_Ext = PDM_I2Cx_GPIOExt1;
 			pdm_table.I2C_addr_GPIO_Ext = PDM_I2CADDR_GPIOExt1;
 			pdm_table.pin_EN_eFuse = TCA9539_IO_P02;			//EN_eFusee_in_ch1 (U25)
 			pdm_table.pin_PG_eFuse = TCA9539_IO_P03;			//PG_eFusee_in_ch1 (U25)
 
 			//Config Power monitor INA238 for power channel 3
-			pdm_table.I2Cx_PWR_Mon = PDM_I2Cx_PWR_Mon_Ch3;
 			pdm_table.I2C_addr_PWR_Mon = PDM_I2CADDR_PWR_Mon_Ch3;
 
 			//Config Power monitor INA238, same for all power channels
@@ -102,19 +96,16 @@ _PDM_table PDM__Table(uint8_t number_pwr_channel){
 
 		case PDM_PWR_Channel_4:
 			//Config I2C GPIO Expander TCA9539 for power channel 4
-			pdm_table.I2Cx_I2C_MUX = PDM_I2Cx_I2C_MUX2;
 			pdm_table.I2C_addr_I2C_MUX = PDM_I2CADDR_I2C_MUX2;
 			//Config I2C MUX for power channel 1
 			pdm_table.I2C_MUX_Ch = TCA9548_CH1;
 
 			//Config I2C GPIO Expander TCA9539 for power channel 2
-			pdm_table.I2Cx_GPIO_Ext = PDM_I2Cx_GPIOExt2;
 			pdm_table.I2C_addr_GPIO_Ext = PDM_I2CADDR_GPIOExt2;
 			pdm_table.pin_EN_eFuse = TCA9539_IO_P02;			//EN_eFusee_in_ch2 (U20)
 			pdm_table.pin_PG_eFuse = TCA9539_IO_P03;			//PG_eFusee_in_ch2 (U20)
 
 			//Config Power monitor INA231 for power channel 2
-			pdm_table.I2Cx_PWR_Mon = PDM_I2Cx_PWR_Mon_Ch4;
 			pdm_table.I2C_addr_PWR_Mon = PDM_I2CADDR_PWR_Mon_Ch4;
 
 			//Config Power monitor INA231, same for all power channels
@@ -131,19 +122,16 @@ _PDM_table PDM__Table(uint8_t number_pwr_channel){
 
 		case PDM_PWR_Channel_5:
 			//Config I2C GPIO Expander TCA9539 for power channel 5
-			pdm_table.I2Cx_I2C_MUX = PDM_I2Cx_I2C_MUX1;
 			pdm_table.I2C_addr_I2C_MUX = PDM_I2CADDR_I2C_MUX1;
 			//Config I2C MUX for power channel 5
 			pdm_table.I2C_MUX_Ch = TCA9548_CH2;
 
 			//Config I2C GPIO Expander TCA9539 for power channel 5
-			pdm_table.I2Cx_GPIO_Ext = PDM_I2Cx_GPIOExt1;
 			pdm_table.I2C_addr_GPIO_Ext = PDM_I2CADDR_GPIOExt1;
 			pdm_table.pin_EN_eFuse = TCA9539_IO_P04;			//EN_eFusee_in_ch1 (U25)
 			pdm_table.pin_PG_eFuse = TCA9539_IO_P05;			//PG_eFusee_in_ch1 (U25)
 
 			//Config Power monitor INA238 for power channel 5
-			pdm_table.I2Cx_PWR_Mon = PDM_I2Cx_PWR_Mon_Ch5;
 			pdm_table.I2C_addr_PWR_Mon = PDM_I2CADDR_PWR_Mon_Ch5;
 
 			//Config Power monitor INA238, same for all power channels
@@ -160,20 +148,17 @@ _PDM_table PDM__Table(uint8_t number_pwr_channel){
 
 		case PDM_PWR_Channel_6:
 			//Config I2C GPIO Expander TCA9539 for power channel 6
-			pdm_table.I2Cx_I2C_MUX = PDM_I2Cx_I2C_MUX2;
 			pdm_table.I2C_addr_I2C_MUX = PDM_I2CADDR_I2C_MUX2;
 			//Config I2C MUX for power channel 6
 			pdm_table.I2C_MUX_Ch = TCA9548_CH2;
 
 
 			//Config I2C GPIO Expander TCA9539 for power channel 6
-			pdm_table.I2Cx_GPIO_Ext = PDM_I2Cx_GPIOExt2;
 			pdm_table.I2C_addr_GPIO_Ext = PDM_I2CADDR_GPIOExt2;
 			pdm_table.pin_EN_eFuse = TCA9539_IO_P04;			//EN_eFusee_in_ch6 (U20)
 			pdm_table.pin_PG_eFuse = TCA9539_IO_P05;			//PG_eFusee_in_ch6 (U20)
 
 			//Config Power monitor INA238 for power channel 2
-			pdm_table.I2Cx_PWR_Mon = PDM_I2Cx_PWR_Mon_Ch6;
 			pdm_table.I2C_addr_PWR_Mon = PDM_I2CADDR_PWR_Mon_Ch6;
 
 			//Config Power monitor INA238, same for all power channels
@@ -190,19 +175,16 @@ _PDM_table PDM__Table(uint8_t number_pwr_channel){
 
 		case PDM_PWR_Channel_7:
 			//Config I2C GPIO Expander TCA9539 for power channel 7
-			pdm_table.I2Cx_I2C_MUX = PDM_I2Cx_I2C_MUX1;
 			pdm_table.I2C_addr_I2C_MUX = PDM_I2CADDR_I2C_MUX1;
 			//Config I2C MUX for power channel 7
 			pdm_table.I2C_MUX_Ch = TCA9548_CH3;
 
 			//Config I2C GPIO Expander TCA9539 for power channel 7
-			pdm_table.I2Cx_GPIO_Ext = PDM_I2Cx_GPIOExt1;
 			pdm_table.I2C_addr_GPIO_Ext = PDM_I2CADDR_GPIOExt1;
 			pdm_table.pin_EN_eFuse = TCA9539_IO_P06;			//EN_eFusee_in_ch7 (U25)
 			pdm_table.pin_PG_eFuse = TCA9539_IO_P07;			//PG_eFusee_in_ch7 (U25)
 
 			//Config Power monitor INA238 for power channel 7
-			pdm_table.I2Cx_PWR_Mon = PDM_I2Cx_PWR_Mon_Ch7;
 			pdm_table.I2C_addr_PWR_Mon = PDM_I2CADDR_PWR_Mon_Ch7;
 
 			//Config Power monitor INA238, same for all power channels
@@ -219,19 +201,16 @@ _PDM_table PDM__Table(uint8_t number_pwr_channel){
 
 		case PDM_PWR_Channel_8:
 			//Config I2C GPIO Expander TCA9539 for power channel 8 wired channel
-			pdm_table.I2Cx_I2C_MUX = PDM_I2Cx_I2C_MUX2;
 			pdm_table.I2C_addr_I2C_MUX = PDM_I2CADDR_I2C_MUX2;
 			//Config I2C MUX for power channel 8
 			pdm_table.I2C_MUX_Ch = TCA9548_CH3;
 
 			//Config I2C GPIO Expander TCA9539 for power channel 8
-			pdm_table.I2Cx_GPIO_Ext = PDM_I2Cx_GPIOExt2;
 			pdm_table.I2C_addr_GPIO_Ext = PDM_I2CADDR_GPIOExt2;
 			pdm_table.pin_EN_eFuse = TCA9539_IO_P06;			//EN_eFusee_in_ch8 (U20)
 			pdm_table.pin_PG_eFuse = TCA9539_IO_P07;			//PG_eFusee_in_ch8 (U20)
 
 			//Config Power monitor INA238 for power channel 2
-			pdm_table.I2Cx_PWR_Mon = PDM_I2Cx_PWR_Mon_Ch8;
 			pdm_table.I2C_addr_PWR_Mon = PDM_I2CADDR_PWR_Mon_Ch8;
 
 			//Config Power monitor INA238, same for all power channels
@@ -248,19 +227,16 @@ _PDM_table PDM__Table(uint8_t number_pwr_channel){
 
 		case PDM_PWR_Channel_9:
 			//Config I2C GPIO Expander TCA9539 for power channel 9 wired channel
-			pdm_table.I2Cx_I2C_MUX = PDM_I2Cx_I2C_MUX1;
 			pdm_table.I2C_addr_I2C_MUX = PDM_I2CADDR_I2C_MUX1;
 			//Config I2C MUX for power channel 9
 			pdm_table.I2C_MUX_Ch = TCA9548_CH4;
 
 			//Config I2C GPIO Expander TCA9539 for power channel 9
-			pdm_table.I2Cx_GPIO_Ext = PDM_I2Cx_GPIOExt1;
 			pdm_table.I2C_addr_GPIO_Ext = PDM_I2CADDR_GPIOExt1;
 			pdm_table.pin_EN_eFuse = TCA9539_IO_P10;			//EN_eFusee_in_ch9 (U25)
 			pdm_table.pin_PG_eFuse = TCA9539_IO_P11;			//PG_eFusee_in_ch9 (U25)
 
 			//Config Power monitor INA238 for power channel 9
-			pdm_table.I2Cx_PWR_Mon = PDM_I2Cx_PWR_Mon_Ch9;
 			pdm_table.I2C_addr_PWR_Mon = PDM_I2CADDR_PWR_Mon_Ch9;
 
 			//Config Power monitor INA238, same for all power channels
@@ -277,19 +253,16 @@ _PDM_table PDM__Table(uint8_t number_pwr_channel){
 
 		case PDM_PWR_Channel_10:
 			//Config I2C GPIO Expander TCA9539 for power channel 10 wired channel
-			pdm_table.I2Cx_I2C_MUX = PDM_I2Cx_I2C_MUX2;
 			pdm_table.I2C_addr_I2C_MUX = PDM_I2CADDR_I2C_MUX2;
 			//Config I2C MUX for power channel 10
 			pdm_table.I2C_MUX_Ch = TCA9548_CH4;
 
 			//Config I2C GPIO Expander TCA9539 for power channel 10
-			pdm_table.I2Cx_GPIO_Ext = PDM_I2Cx_GPIOExt2;
 			pdm_table.I2C_addr_GPIO_Ext = PDM_I2CADDR_GPIOExt2;
 			pdm_table.pin_EN_eFuse = TCA9539_IO_P10;			//EN_eFusee_in_ch10 (U20)
 			pdm_table.pin_PG_eFuse = TCA9539_IO_P11;			//PG_eFusee_in_ch10 (U20)
 
 			//Config Power monitor INA238 for power channel 10
-			pdm_table.I2Cx_PWR_Mon = PDM_I2Cx_PWR_Mon_Ch10;
 			pdm_table.I2C_addr_PWR_Mon = PDM_I2CADDR_PWR_Mon_Ch10;
 
 			//Config Power monitor INA238, same for all power channels
@@ -306,19 +279,16 @@ _PDM_table PDM__Table(uint8_t number_pwr_channel){
 
 		case PDM_PWR_Channel_11:
 			//Config I2C GPIO Expander TCA9539 for power channel 11 wired channel
-			pdm_table.I2Cx_I2C_MUX = PDM_I2Cx_I2C_MUX1;
 			pdm_table.I2C_addr_I2C_MUX = PDM_I2CADDR_I2C_MUX1;
 			//Config I2C MUX for power channel 11
 			pdm_table.I2C_MUX_Ch = TCA9548_CH5;
 
 			//Config I2C GPIO Expander TCA9539 for power channel 11
-			pdm_table.I2Cx_GPIO_Ext = PDM_I2Cx_GPIOExt1;
 			pdm_table.I2C_addr_GPIO_Ext = PDM_I2CADDR_GPIOExt1;
 			pdm_table.pin_EN_eFuse = TCA9539_IO_P12;			//EN_eFusee_in_ch11 (U25)
 			pdm_table.pin_PG_eFuse = TCA9539_IO_P13;			//PG_eFusee_in_ch11 (U25)
 
 			//Config Power monitor INA238 for power channel 11
-			pdm_table.I2Cx_PWR_Mon = PDM_I2Cx_PWR_Mon_Ch11;
 			pdm_table.I2C_addr_PWR_Mon = PDM_I2CADDR_PWR_Mon_Ch11;
 
 			//Config Power monitor INA238, same for all power channels
@@ -335,19 +305,16 @@ _PDM_table PDM__Table(uint8_t number_pwr_channel){
 
 		case PDM_PWR_Channel_12:
 			//Config I2C GPIO Expander TCA9539 for power channel 12 High current channel
-			pdm_table.I2Cx_I2C_MUX = PDM_I2Cx_I2C_MUX2;
 			pdm_table.I2C_addr_I2C_MUX = PDM_I2CADDR_I2C_MUX2;
 			//Config I2C MUX for power channel 12
 			pdm_table.I2C_MUX_Ch = TCA9548_CH5;
 
 			//Config I2C GPIO Expander TCA9539 for power channel 12
-			pdm_table.I2Cx_GPIO_Ext = PDM_I2Cx_GPIOExt2;
 			pdm_table.I2C_addr_GPIO_Ext = PDM_I2CADDR_GPIOExt2;
 			pdm_table.pin_EN_eFuse = TCA9539_IO_P12;			//EN_eFusee_in_ch12 (U20)
 			pdm_table.pin_PG_eFuse = TCA9539_IO_P13;			//PG_eFusee_in_ch12 (U20)
 
 			//Config Power monitor INA231 for power channel 12
-			pdm_table.I2Cx_PWR_Mon = PDM_I2Cx_PWR_Mon_Ch12;
 			pdm_table.I2C_addr_PWR_Mon = PDM_I2CADDR_PWR_Mon_Ch12;
 
 			//Config Power monitor INA231, same for all power channels
